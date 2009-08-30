743a0f4c5d6a5ded19b91f06b92836fad89bade6
Kerry Buckley <kerryjbuckley@gmail.com>
2009-08-30 19:27:45 +0100

    Tidied up spec

--- spec/kerryb/kerryb_spec.rb
+++ spec/kerryb/kerryb_spec.rb
@@ -2,11 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 require 'kerryb/kerryb'
 
 describe Kerryb::Kerryb do
-
-  it "should be instantiable with no paramters" do
-
+  it "is instantiable with no parameters" do
     lambda { Kerryb::Kerryb.new }.should_not raise_error
-
   end
-
 end
