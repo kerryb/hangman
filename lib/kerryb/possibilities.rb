72c80df5c2443771780767760e7a403c3bbd2218
Kerry Buckley <kerryjbuckley@gmail.com>
2009-09-13 10:41:15 +0100

    Reworked choosing algorithm.
    Now gives ten points for each word a letter appears in, and one
    point for each overall occurrence, then chooses the one with most
    points.

--- lib/kerryb/possibilities.rb
+++ lib/kerryb/possibilities.rb
@@ -8,8 +8,7 @@ module Kerryb
     def suggest_guess pattern
       regexp = /^#{pattern.gsub '_', "[#{@letters.join ''}]"}$/
       @words.reject! {|word| word !~ regexp}
-      letter_counts = count_letter_appearances
-      choose_letter letter_counts
+      choose_letter
     end
 
     def letter_used letter
@@ -18,38 +17,20 @@ module Kerryb
 
     private
 
-    def count_letter_appearances
-      letter_counts = {}
-      @letters.each do |letter|
-        letter_counts[letter] = count_words_containing_letter letter
+    def choose_letter
+      winner = @letters.inject([nil, 0]) do |choice, letter|
+        score = 10 * words_containing(letter) + total_occurences_of(letter)
+        score > choice[1] ? [letter, score] : choice
       end
-      return letter_counts
+      winner[0]
     end
 
-    def count_words_containing_letter letter
+    def words_containing letter
       @words.select {|word| word.include? letter}.size
     end
 
-    def choose_letter letter_counts
-      sorted_letter_counts = sort_letter_counts letter_counts
-      highest_count = sorted_letter_counts.last[1]
-      letters_in_most_words = sorted_letter_counts.select {|letter_count| letter_count[1] == highest_count}.map {|letter_count| letter_count[0]}
-      if letters_in_most_words.size == 1
-        letters_in_most_words.first
-      else
-        most_common_letter letters_in_most_words
-      end
-    end
-
-    def most_common_letter letters
-      concatenated_words = @words.join('')
-      letter_counts = letters.map {|letter| [letter, concatenated_words.count(letter)]}
-      sorted_letter_counts = sort_letter_counts letter_counts
-      sorted_letter_counts.last[0]
-    end
-
-    def sort_letter_counts letter_counts
-      letter_counts.sort {|first, second| first[1] <=> second[1]}
+    def total_occurences_of letter
+      @words.join('').count letter
     end
   end
 end
