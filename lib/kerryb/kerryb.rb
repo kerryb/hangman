0df6b8b8da24fa3b87ecb31370bc1168a503906f
Kerry Buckley <kerryjbuckley@gmail.com>
2009-09-13 11:36:59 +0100

    Moved everything into one class

--- lib/kerryb/kerryb.rb
+++ lib/kerryb/kerryb.rb
@@ -1,31 +1,50 @@
-require 'kerryb/possibilities'
-
 module Kerryb
   class Kerryb
     def word_list=(list)
-      @word_list = list
+      @all_words = list
     end
 
     def new_game(guesses_left)
-      @possibilities = Possibilities.new @word_list
+      @words = @all_words.dup
+      @letters = ('a'..'z').to_a
     end
 
     def guess(word, guesses_left)
-      @possibilities.suggest_guess word
+      regexp = /^#{word.gsub '_', "[#{@letters.join ''}]"}$/
+      @words.reject! {|word| word !~ regexp}
+      choose_letter
     end
 
     def incorrect_guess(guess)
-      @possibilities.letter_used guess
+      @letters.delete guess
     end
 
     def correct_guess(guess)
-      @possibilities.letter_used guess
+      @letters.delete guess
     end
 
     def fail(reason)
     end
 
     def game_result(result, word)
+    end
+
+    private
+
+    def choose_letter
+      winner = @letters.inject([nil, 0]) do |choice, letter|
+        score = 10 * words_containing(letter) + total_occurences_of(letter)
+        score > choice[1] ? [letter, score] : choice
+      end
+      winner[0]
+    end
+
+    def words_containing letter
+      @words.select {|word| word.include? letter}.size
+    end
+
+    def total_occurences_of letter
+      @words.join('').count letter
     end
   end
 end
--- spec/kerryb/kerryb_spec.rb
+++ spec/kerryb/kerryb_spec.rb
@@ -1,42 +1,55 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 require 'kerryb/kerryb'
-require 'kerryb/possibilities'
 
 describe Kerryb::Kerryb do
   before do
-    @possibilities = stub Kerryb::Possibilities, :null_object => true
-    Kerryb::Possibilities.stub(:new).and_return @possibilities
     @game = Kerryb::Kerryb.new
+    @game.word_list = %w(ant bee ape man)
     @game.new_game 7
-    @words = %w(ant bee)
-    @game.word_list = @words
   end
 
-  describe 'starting a new game' do
-    it 'creates a new possibilities set' do
-      Kerryb::Possibilities.should_receive(:new).with @words
-      @game.new_game 7
+  describe 'suggesting a letter to guess' do
+    it 'provides the letter which appears in the most words' do
+      @game.guess('___', 5).should == 'a'
+    end
+
+    it "ignores potential words which don't match" do
+      @game.guess('b__', 5).should == 'e'
+    end
+
+    it 'ignores letters that have already been correctly guessed' do
+      @game.correct_guess 'a'
+      @game.guess('___', 5).should == 'e'
+    end
+
+    it 'ignores letters that have already been incorrectly guessed' do
+      @game.incorrect_guess 'a'
+      @game.guess('___', 5).should == 'e'
+    end
+
+    describe 'when two letters appear in the same number of words' do
+      it 'provides the one that appears the most times' do
+        @game.word_list = %w(ant bee ape)
+        @game.new_game 7
+        @game.guess('___', 5).should == 'e'
+      end
     end
   end
 
-  describe 'making a guess' do
-    it 'uses the suggested guess from the word list' do
-      @possibilities.stub(:suggest_guess).with('f__').and_return 'x'
-      @game.guess('f__', 7).should == 'x'
-    end
-  end
-
-  describe 'after an incorrect guess' do
-    it 'marks the letter as used' do
-      @possibilities.should_receive(:letter_used).with 'z'
-      @game.incorrect_guess 'z'
-    end
-  end
-
-  describe 'after a correct guess' do
-    it 'marks the letter as used' do
-      @possibilities.should_receive(:letter_used).with 'z'
-      @game.correct_guess 'z'
+  describe 'Playing a game' do
+    it 'all turns out much as expected' do
+      game = Kerryb::Kerryb.new
+      game.word_list = %w(ant bee ape aardvark)
+      game.new_game 5
+      game.guess('___', 5).should == 'e'
+      game.incorrect_guess 'e'
+      game.guess('___', 4).should == 'a'
+      game.correct_guess 'a'
+      game.guess('a__', 3).should == 'n'
+      game.correct_guess 'n'
+      game.guess('an_', 3).should == 't'
+      game.correct_guess 't'
+      game.game_result 'win', 'ant'
     end
   end
 end
